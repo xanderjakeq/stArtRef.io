b9a3a15b374e73938739c86056d3e5dec4307d73
Xander Jake de los Santos <quanjake@gmail.com>
2018-10-31 16:46:19 -0400

    started backend

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,12 @@
 const functions = require('firebase-functions');
+const express = require('express');
+import Unsplash, {toJson} from 'unsplash-js';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get('/random-photos', (request, response) =>{
+
+})
+
+
+exports.app = functions.https.onRequest(app);
