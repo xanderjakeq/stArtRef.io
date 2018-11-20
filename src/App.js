d183e686c5652e221d46caf5637ced6f4c8708a1
Xander Jake de los Santos <quanjake@gmail.com>
2018-11-20 00:04:15 -0500

    changed fetch destination

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">stArtref</h1>
+          <h1 className="App-title">stArtRef</h1>
           <p>
             Finish the Scribble <br/>
             Draw something new with 2-3 refs.
