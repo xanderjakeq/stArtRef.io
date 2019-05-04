c766be0098bc7ab6a0817d1f21b41d62a33fa75d
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-04 14:27:44 -0400

    Refactored to use react hooks

--- src/components/Authentication/Authentication.js
+++ src/components/Authentication/Authentication.js
@@ -1,63 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect} from 'react';
 
 import './Authentication.css';
 
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
 
-import UserProfile from '../UserProfile/UserProfile'
+import UserProfile from '../UserProfile/UserProfile';
 
-
-class Authentication extends Component {
-
-  constructor(props){
-    super(props)
-    // The component's Local state.
-    this.state = {
-      isSignedIn: false // Local signed-in state.
-    };
-  }
-
-
-  // Configure FirebaseUI.
-  uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: 'redirect',
-    // We will display Google and Facebook as auth providers.
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-      // Avoid redirects after sign-in.
-      signInSuccessWithAuthResult: () => false
-    }
-  };
-
-  // Listen to the Firebase Auth state and set the local state.
-  componentDidMount() {
-    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
-        (user) => this.setState({isSignedIn: !!user})
-    );
-  }
-  
-  // Make sure we un-register Firebase observers when the component unmounts.
-  componentWillUnmount() {
-    this.unregisterAuthObserver();
+const Authentication = (props) => {
     
-  }
+    const [isSignedIn, updateIsSignedIn] = useState(false); 
 
-  render() {
-    if (!this.state.isSignedIn) {
+    // Configure FirebaseUI.
+    const uiConfig = {
+        // Popup signin flow rather than redirect flow.
+        signInFlow: 'redirect',
+        // We will display Google and Facebook as auth providers.
+        signInOptions: [
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+        callbacks: {
+          // Avoid redirects after sign-in.
+          signInSuccessWithAuthResult: () => false
+        }
+    };
+
+    useEffect(()=>{
+        return isUserAuthed();
+    });
+
+    if (!isSignedIn) {
       return (
         <div id= "login-form-container">
           <h1>stArtRef</h1>
-          <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
+          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
           <p>If its the first time, an account will be created for u</p>
           <hr/>
           <p>Dunno how to comply to GDPR so uhh.. if u make an account, u agree that u will not sue me.</p>
           <br/>
           <h1>( ˘ ³˘)❤</h1>
-
         </div>
       );
     }else{
@@ -67,7 +48,16 @@ class Authentication extends Component {
         </div>
       );
     }
-  }
+
+    // Listen to the Firebase Auth state and set the local state.
+    function isUserAuthed() {
+        const unregisterAuthObserver = firebase.auth().onAuthStateChanged(
+            (user) => updateIsSignedIn(!!user)
+        );
+
+        // Make sure we un-register Firebase observers when the component unmounts.
+        return unregisterAuthObserver(); 
+    }
 }
 
 export default Authentication;
