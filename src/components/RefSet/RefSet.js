c8f342d52213e769ca97cfcaac16012d6759de9f
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-27 00:58:02 -0400

    Add upload functionality

--- src/components/RefSet/RefSet.js
+++ src/components/RefSet/RefSet.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Link} from "react-router-dom";
 import firebase from 'firebase';
+import styled from 'styled-components';
 import { toJson } from "unsplash-js";
 
 import './RefSet.css';
@@ -8,118 +9,134 @@ import './RefSet.css';
 import Ref from '../Ref/Ref'
 import Scribble from '../Scribble/Scribble'
 
-class RefSet extends Component {
-    constructor(props){
-        super(props)
+const RefSet = (props) => {
 
-        this.state = {
-            user: firebase.auth().currentUser,
-            username: '',
-            artLink: '',
-            active: false
-        }
+    const [user, setUser] = useState();
+    const [username, setUserName] = useState('');
+    const [active, setActive] = useState(false);
+    const [upload, setUpload] = useState(null);
 
-        this.handleInputChange = this.handleInputChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+	const storageRef = firebase.storage().ref();
 
-    // componentDidMount(){
-    //     firebase.auth().onAuthStateChanged(
-    //       (user) => {
-    //         if(user){
-    //           this.setState({
-    //             user: user,
-    //             username: this.state.userDBRef.child('Users/' + user.uid).once('value').then((snap) => {return snap.val().username})
-    //           })
-    //         }
-    //       }
-    //     )
-    //   }
+	useEffect(() => {
+        setUser(firebase.auth().currentUser);
+	},[])
 
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        this.setState({
-          [name]: value
-        });
-      }
+    useEffect(()=>{
+		setUpload(document.getElementById('real-file'));
+    },[user]);
 
-    async handleSubmit(){
-
-        if(this.state.artLink.indexOf('https://www.instagram.com/p/') === -1){
-            alert("pls make sure its from instagram \n (灬♥ω♥灬)")
-            return
-        }else{
-
-            let artLink = this.state.artLink.substring(0, this.state.artLink.indexOf('?') !== -1 ? this.state.artLink.indexOf('?'): this.state.artLink.lastIndexOf('/'))
-
-            await fetch(artLink + '?__a=1')
-            .then(toJson)
-              .then(json => {
-                // let imgLink =  
-                artLink = json.graphql.shortcode_media.display_url 
-                //.substring(0, imgLink.indexOf('?') !== -1 ? imgLink.indexOf('?'): imgLink.length)
-              });
-
-            let username = await firebase.database().ref().child('Users/' + this.state.user.uid).once('value').then((snap) => {return snap.val().username})
-            let postKey = firebase.database().ref().child('UserGroupedPosts/' + this.state.user.uid).push({
-                author: username,
-                artLink: artLink,
-                refLinks: this.props.data
-            }).key
-
-            firebase.database().ref().child('Posts/').update({
-                [postKey]: {
-                    author: username,
-                    artLink: artLink,
-                    refLinks: this.props.data
-                }
-            })
-
-            firebase.database().ref().child('UserGroupedRefs/' + this.state.user.uid + '/' + this.props.refKey).remove()
-        }
-    }
-
-
-    render(){
-        return(
+    return(
+        <>
+            <HiddenInput type = 'file' id = 'real-file' hidden = 'hidden' onChange = {handleFile}/>
             <div>
                 <div className = 'inputWrapper'>
                 <label className = 'formItem'>Instagram share link</label>
-                <input
-                    name="artLink"
-                    type="text"
-                    value={this.state.artLink}
-                    onChange={this.handleInputChange} 
-                    className = 'formItem'
-                    />
+                <UploadBtn onClick = {handleClick}>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M17 8L12 3L7 8" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12 3V15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </UploadBtn>
                 </div>
-            <div className = "refSetWrapper">
-                <div>
-                    
+                <div className = "refSetWrapper">
                     <div className="refSet">
-                        <Ref photoInfo = {this.props.data[0]}/>
-                        <Ref photoInfo = {this.props.data[2]}/>
-                        <Scribble scribbleUrl = {this.props.data[1]}/>
+                        <Ref photoInfo = {props.data[0]}/>
+                        <Ref photoInfo = {props.data[2]}/>
+                        <Scribble scribbleUrl = {props.data[1]}/>
                     </div>
-                    
-                    <div  className = "uploadButtonWrapper">
-
-                    {this.state.artLink !== '' &&
-                    <Link to = "/explore">
-                        <button onClick = {this.handleSubmit} id = "submit" className = "generate-btn">
-                            <span className = "uploadButtonIcons" role = "img" aria-label = "post">📤</span>
-                        </button>
-                    </Link>
-                    }
-                    </div>
-                </div>    
+                </div>
             </div>
+        </>
+    )
 
-            </div>
-        )
-      }
+    function handleClick(e){
+		upload.click();
+	}
+
+	// referenced https://firebase.google.com/docs/storage/web/upload-files
+	function handleFile(e){
+		const input = e.target;
+		const file = input.files[0];
+		if(file.name.includes('.png')){
+			const uploadTask = storageRef.child(`audio/${file.name}`).put(file);
+			uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, 
+				(snapshot) => {
+					// Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+					var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+					console.log('Upload is ' + progress + '% done');
+					switch (snapshot.state) {
+					case firebase.storage.TaskState.PAUSED: // or 'paused'
+						console.log('Upload is paused');
+						break;
+					case firebase.storage.TaskState.RUNNING: // or 'running'
+						console.log('Upload is running');
+						break;
+					default:
+						console.log(snapshot)
+					}
+				}, (error) => {
+
+				switch (error.code) {
+					case 'storage/unauthorized':
+					// User doesn't have permission to access the object
+					break;
+
+					case 'storage/canceled':
+					// User canceled the upload
+					break;
+					case 'storage/unknown':
+					// Unknown error occurred, inspect error.serverResponse
+					break;
+					default:
+						console.log(error)
+				}
+				}, () => {
+					uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+						console.log('File available at', downloadURL);
+                        handleSubmit(downloadURL);
+					});
+				}
+			)
+		}else{
+			console.log('select an mp3 file')
+		}
+
+		console.log(input.files)
+	}
+
+    async function handleSubmit(artUrl){
+        let username = await firebase.database().ref().child('Users/' + user.uid).once('value').then((snap) => {return snap.val().username})
+        let postKey = firebase.database().ref().child('UserGroupedPosts/' + user.uid).push({
+            author: username,
+            artLink: artUrl,
+            refLinks: props.data
+        }).key
+
+        firebase.database().ref().child('Posts/').update({
+            [postKey]: {
+                author: username,
+                artLink: artUrl,
+                refLinks: props.data
+            }
+        })
+
+        firebase.database().ref().child('UserGroupedRefs/' + user.uid + '/' + props.refKey).remove()
+    }
 }
 
 export default RefSet;
+
+const HiddenInput = styled.input`
+    display: none;
+`;
+
+const UploadBtn = styled.button`
+	background: none; 
+	border: none;
+	outline: none;
+	:hover{
+		cursor: pointer;
+	}
+`;
