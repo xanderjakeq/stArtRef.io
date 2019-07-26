75dabad3d258b913f45e1992a30dc2b1245a5d21
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-26 02:41:50 -0400

    Create Redux store.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
-import * as firebase from 'firebase';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers';
 
+const store = createStore(
+	rootReducer,
+	applyMiddleware(thunk)
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
 // registerServiceWorker();
