8297aeb56177d2d72ef5b8a5e8481a6fe547907c
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-26 22:58:52 -0400

    Use Ref style for scribble

--- src/components/Scribble/Scribble.js
+++ src/components/Scribble/Scribble.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import {Ref} from '../Ref/Ref';
+
 const Scribble = (props) => {
 
     return(
-        <div className="refs scribble">
+        // <div className="refs scribble">
+        <Ref>
             <a href = {'https://www.instagram.com/startrefio/'} target="_blank"><img src = {props.scribbleUrl} alt = "Scribble"></img></a>
             {/* <p className = "author">by <a href={props.photoInfo.user.links.html + "?utm_source=startref&utm_medium=referral"} target="_blank"> {props.photoInfo.user.name} </a></p> */}
-        </div>
+        </Ref>
     );
 }
 
