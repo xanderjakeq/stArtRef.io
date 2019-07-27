a218a18c01152c024b7ca44619caa7951ebe0af4
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-26 23:35:58 -0400

    Updated NavBar styles with styled components

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,12 +1,14 @@
 import React, {  } from 'react';
 import {Link} from "react-router-dom";
 
+import styled from 'styled-components';
+
 import './NavBar.css';
 import logo from '../../logo.svg';
 
 const NavBar = (props) => {
     return(
-        <div className="startref_nav" >
+        <Nav>
             <Link to = "/">        
                 <img src={logo} alt="startref logo" />
             </Link>
@@ -14,8 +16,56 @@ const NavBar = (props) => {
                 <Link to = "/explore"><span role = "img" className = "navIcons" aria-label = "explore">🖼️</span></Link>
                 <Link to = "/profile"><span role = "img" className = "navIcons" aria-label = "profile">🙈</span></Link>
             </div>
-        </div>
+        </Nav>
     )
 }
 
 export default NavBar;
+
+const Nav = styled.div`
+    position: fixed;
+    top:0;
+    z-index: 10;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    height: 60px;
+    background-color: #fff;
+    font-size: 25px;
+
+    border-bottom: 1px solid rgba(0,0,0,.0975);
+  
+    img {
+        width: 50px;
+        /* to push left border */
+        padding-left: 20%;
+    }
+    
+    .insta_text {
+        padding-left: 10px;
+    }
+    
+    a{
+        text-decoration: none;
+        display: inline;
+        padding-right: 10px;
+    }
+
+    .navIcons{
+        font-size: 30px;
+        vertical-align: middle;
+        line-height: 2;
+    }
+
+    @media only screen and (min-width: 600px) {
+        padding: 5px 0;
+        border-bottom: 1px solid rgba(0,0,0,.0975);
+        img {
+            width: 100px;
+        }
+        .navIcons{
+            font-size: 50px;
+        }
+    }
+`;
