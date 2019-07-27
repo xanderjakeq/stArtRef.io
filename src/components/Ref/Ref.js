c6a48669928742402665427c3e8cf99a4b621cfb
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-26 22:57:59 -0400

    Update Ref style with styled components

--- src/components/Ref/Ref.js
+++ src/components/Ref/Ref.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+export const Ref = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 20px;
+    width: 300px;
+    height: min-content;
+    a {
+        width: 100%;
+        img {
+            width: 100%;
+        }
+    }
+`;
+
 const Reference = (props) => {
 
     return(
-        <div className="refs">
+        <Ref>
             <a href = {props.photoInfo.links.html} target="_blank"><img src = {props.photoInfo.urls.small} alt = "Reference"></img></a>
             <p className = "author">by <a href={props.photoInfo.user.links.html + "?utm_source=startref&utm_medium=referral"} target="_blank"> {props.photoInfo.user.name} </a></p>
-        </div>
+        </Ref>
     );
 }
 
