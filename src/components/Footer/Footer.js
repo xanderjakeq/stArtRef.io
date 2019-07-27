c6247f5872df1ee0e571aa46c0077d4d376d8ca0
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-27 00:15:06 -0400

    Update Footer style with styled-components

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Footer = (props) => {
     return(
-        <footer className = {props.className}>
+        <Footr>
             <h4>
                 Made with 
                 <span role="img" aria-label="Heart">❤️</span> 
                 by <a href="https://medium.com/@xanderjakeq" target="_blank" rel="noopener noreferrer">xanderjakeq</a>
             </h4>
-        </footer>
+        </Footr>
     );
 }
 
 export default Footer;
+
+const Footr = styled.footer`
+    /* position: fixed; */
+    /* width: 100%;
+    height: 10vh; */
+    height: var(--footer-height);
+    /* bottom: 0px; */
+    position: relative;
+    bottom: 0;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    background-color: rgb(70,83,98, .9);
+    a{
+        color:#06ffc7;
+    }
+`;
