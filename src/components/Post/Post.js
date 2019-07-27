c8e1e00eb83f440f006f3a026e601dfa24c9d85c
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-27 02:44:59 -0400

    Restructure to be more robust and reusable

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,11 +1,27 @@
 import React, { } from 'react';
-import './Post.css';
+import {Link} from 'react-router-dom';
+
+import styled from 'styled-components';
 
 const Post = (props) => {
     return( 
-        <img className = "post" src={props.post.data.artLink} onClick = {props.onClick(props.post)} alt = 'art'/>       
+        <Wrapper>
+            <Link to = {props.url}>
+                <Art src={props.post.data.artLink} onClick = {props.click(props.post)} alt = 'art'/>       
+            </Link>
+        </Wrapper>
     )
 }
 
-
 export default Post;
+
+const Wrapper = styled.div`
+    width: calc(100%/5);
+    margin: 10px; 
+`;
+
+const Art = styled.img`
+    display: block;
+    width: 100%;
+    height: 100%;
+`;
