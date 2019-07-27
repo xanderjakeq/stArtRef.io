1cadcb8eed1f32daeb5b4296436f6e7a77b2aa52
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-26 20:57:12 -0400

    Implement signout()

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,10 +1,20 @@
 import {ERROR} from './index';
 
+import firebase from '../components/config/firebaseApp';
+
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const SIGNOUT = "SIGNOUT";
 
 export const savedata = data => {
 	return {
 		type: LOGIN_SUCCESS,
 		payload: data
+	}
+}
+
+export const signout = () => {
+	firebase.auth().signOut();
+	return {
+		type: SIGNOUT
 	}
 }
