595a168c44bf8c409b6e9efb7d04b4dd61683d91
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-26 21:33:40 -0400

    Refactor to use higher order function
    So that dispatch() can be used for
    fetching userData

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,15 +1,26 @@
 import {ERROR} from './index';
 
 import firebase from '../components/config/firebaseApp';
+import { dispatch } from '../../../../../../Users/xanderjakeq/AppData/Local/Microsoft/TypeScript/3.5/node_modules/rxjs/internal/observable/range';
 
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const SIGNOUT = "SIGNOUT";
 
-export const savedata = data => {
-	return {
-		type: LOGIN_SUCCESS,
-		payload: data
-	}
+const database = firebase.database().ref();
+
+export const savedata = user => dispatch => {
+	database.child('Users/' + user.uid).on('value', snap => {
+		let val = snap.val();
+		if(val !== null){
+			dispatch({
+				type: LOGIN_SUCCESS,
+				payload: {
+					user,
+					userData: val
+				}
+			});
+		}
+	});
 }
 
 export const signout = () => {
