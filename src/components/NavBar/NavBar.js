de40a18acbd8294ac064a3188cd0b4a82809dc98
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-26 22:13:12 -0400

    Clean up
    Remove comments and h1 title

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,23 +10,12 @@ const NavBar = (props) => {
             <Link to = "/">        
                 <img src={logo} alt="startref logo" />
             </Link>
-            <h1 className="appName">stArtRef</h1>
             <div className="startref_icons">
                 <Link to = "/explore"><span role = "img" className = "navIcons" aria-label = "explore">🖼️</span></Link>
                 <Link to = "/profile"><span role = "img" className = "navIcons" aria-label = "profile">🙈</span></Link>
-                {/* <IsLoggedIn isLoggedIn = {props.isLoggedIn} />  */}
             </div>
-            {/* <IsLoggedIn IsLoggedIn = {props.isLoggedIn} /> */}
         </div>
     )
 }
-
-// function IsLoggedIn(props){
-//     if(props.isLoggedIn){
-//         return <Link to = "/profile">Profile</Link>
-//     }else{
-//         return <Link to = "/login">Profile</Link>
-//     }
-// }
 
 export default NavBar;
