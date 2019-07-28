ed045c56e40f9c91ffdf7daf846165b333e7d12c
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-28 19:26:07 -0400

    Set conditional rendering if link is not available

--- src/components/Ref/Ref.js
+++ src/components/Ref/Ref.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import styled from 'styled-components';
 
+import loading from '../../loading.png';
+
 export const Ref = styled.div`
     display: flex;
     flex-direction: column;
@@ -19,12 +21,18 @@ export const Ref = styled.div`
     }
 `;
 
+
 const Reference = (props) => {
 
     return(
         <Ref>
-            <a href = {props.photoInfo.links.html} target="_blank"><img src = {props.photoInfo.urls.small} alt = "Reference"></img></a>
-            <p className = "author">by <a href={props.photoInfo.user.links.html + "?utm_source=startref&utm_medium=referral"} target="_blank"> {props.photoInfo.user.name} </a></p>
+            <a href = {props.photoInfo.links.html} target="_blank"><img src = {props.photoInfo.urls.small ? props.photoInfo.urls.small : loading} alt = "Reference"></img></a>
+            {
+                props.photoInfo.urls.small ? 
+                <p className = "author">by <a href={props.photoInfo.user.links.html + "?utm_source=startref&utm_medium=referral"} target="_blank"> {props.photoInfo.user.name} </a></p>
+                :
+                <p className = "author">follow <a href="https://www.patreon.com/xanderjakeq" target="_blank">xanderjakeq</a></p>
+            }
         </Ref>
     );
 }
