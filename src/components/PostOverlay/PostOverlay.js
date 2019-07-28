52328dd64af2cd8ea676ffa216496a8e4e616fed
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-27 21:29:46 -0400

    Fetch ActivePost data if Post is Accessed through direct link.
    Else use existing data.

--- src/components/PostOverlay/PostOverlay.js
+++ src/components/PostOverlay/PostOverlay.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import firebase from '../config/firebaseApp';
 import {connect} from "react-redux";
 
-import Ref from '../Ref/Ref';
-import Scribble from '../Scribble/Scribble';
+import Ref from '../Ref/Ref'
+import Scribble from '../Scribble/Scribble'
 
 import './PostOverlay.css';
 
 const Overlay = (props) => {
-    return (
+
+    const [post, setPost] = useState();
+
+    useEffect(() => {
+        if (Object.entries(props.post).length === 0 && props.post.constructor === Object) {
+            firebase.database().ref().child('Posts/' + props.match.params.postID).once('value', snap => {
+                setPost(snap.val())
+            })
+        }
+        else {
+            setPost(props.post)
+        }
+    },[])
+
+    return post !== undefined ? (
         <div className = "overlayWrapper">
             <h1 className="close-thin" onClick = {() => props.history.goBack()}></h1>
             <div className = "imagesWrapper">
-                <img src = {props.post.artLink} onClick = {() => props.history.push(`/${props.post.author}`)} alt = 'Art'/>
+
+                <img src = {post.artLink} onClick = {() => props.history.push(`/${post.author}`)} alt = 'Art'/>
 
                 <div className="refSet">
-                    <Ref photoInfo = {props.post.refLinks[0]}/>
-                    <Ref photoInfo = {props.post.refLinks[2]}/>
-                    <Scribble scribbleUrl = {props.post.refLinks[1]}/>
+                    <Ref photoInfo = {post.refLinks[0]}/>
+                    <Ref photoInfo = {post.refLinks[2]}/>
+                    <Scribble scribbleUrl = {post.refLinks[1]}/>
                 </div>
             </div>
         </div>
     ) 
+    :
+    null
 }
 
 const mstp = state => {
