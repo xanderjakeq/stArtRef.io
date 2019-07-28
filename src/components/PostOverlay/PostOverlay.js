dbad9743f67d5c3c3ecef7231543740148bedd06
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-28 00:53:55 -0400

    Update style to use styled-components

--- src/components/PostOverlay/PostOverlay.js
+++ src/components/PostOverlay/PostOverlay.js
@@ -2,10 +2,12 @@ import React, {useState, useEffect} from 'react';
 import firebase from '../config/firebaseApp';
 import {connect} from "react-redux";
 
+import styled from "styled-components";
+
 import Ref from '../Ref/Ref'
 import Scribble from '../Scribble/Scribble'
 
-import './PostOverlay.css';
+// import './PostOverlay.css';
 
 const Overlay = (props) => {
 
@@ -23,8 +25,8 @@ const Overlay = (props) => {
     },[])
 
     return post !== undefined ? (
-        <div className = "overlayWrapper">
-            <h1 className="close-thin" onClick = {() => props.history.goBack()}></h1>
+        <OverlayWrapper>
+            <ClosetBtn className="close-thin" onClick = {() => props.history.goBack()}/>
             <div className = "imagesWrapper">
 
                 <img src = {post.artLink} onClick = {() => props.history.push(`/${post.author}`)} alt = 'Art'/>
@@ -35,7 +37,7 @@ const Overlay = (props) => {
                     <Scribble scribbleUrl = {post.refLinks[1]}/>
                 </div>
             </div>
-        </div>
+        </OverlayWrapper>
     ) 
     :
     null
@@ -48,3 +50,36 @@ const mstp = state => {
 }
 
 export default connect(mstp,{})(Overlay);
+
+const OverlayWrapper = styled.div`
+    position: fixed; /* Sit on top of the page content */
+    /* display: none; Hidden by default */
+    width: 100vw; /* Full width (cover the whole page) */
+    height: 100vh; /* Full height (cover the whole page) */
+    top: 0; 
+    left: 0;
+    /* right: 0;  */
+    bottom: 0;
+    background-color:#55e1d0; /* Black background with opacity */
+    z-index: 10; /* Specify a stack order in case you're using a different order for other elements */
+    // cursor: pointer; /* Add a pointer on hover */
+    // margin-left: -10px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ClosetBtn = styled.div`
+    color: #777;
+    font: 14px/100% arial, sans-serif;
+    position: absolute;
+    right: 5%;
+    text-decoration: none;
+    text-shadow: 0 1px 0 #fff;
+    top: 5px;
+
+    &:after {
+        content: '✖'; /* UTF-8 symbol */
+    }
+`;
