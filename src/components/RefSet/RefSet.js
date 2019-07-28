8169c05bc0c3b6c642e1425f82ad324775a91760
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-28 03:02:00 -0400

    Update UploadBtn style

--- src/components/RefSet/RefSet.js
+++ src/components/RefSet/RefSet.js
@@ -30,16 +30,7 @@ const RefSet = (props) => {
         <>
             <HiddenInput type = 'file' id = 'real-file' hidden = 'hidden' onChange = {handleFile}/>
             <div>
-                <div className = 'inputWrapper'>
-                <label className = 'formItem'>Instagram share link</label>
-                <UploadBtn onClick = {handleClick}>
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M21 15V19C21 19.5304 20.7893 20.0391 20.4142 20.4142C20.0391 20.7893 19.5304 21 19 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M17 8L12 3L7 8" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    <path d="M12 3V15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </UploadBtn>
-                </div>
+                <UploadBtn onClick = {handleClick}>Share Art</UploadBtn>
                 <div className = "refSetWrapper">
                     <RefSetStyle>
                         <Ref photoInfo = {props.data[0]}/>
@@ -133,10 +124,17 @@ const HiddenInput = styled.input`
 `;
 
 const UploadBtn = styled.button`
-	background: none; 
-	border: none;
 	outline: none;
+	background: transparent;
+	border: solid 1px #27a9e0;
+	border-radius: 3px;
+	color: #27a9e0;
+	font-size: 16px;
+	margin: 20px 20px 0;
+	outline: none !important;
+	padding: 10px 20px;
 	:hover{
 		cursor: pointer;
+		background: #FDF9F0;
 	}
 `;
