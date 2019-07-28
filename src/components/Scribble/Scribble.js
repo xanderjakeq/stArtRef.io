59e935a0e6656947852218e9db6d2a954e3fdb6d
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-07-28 19:26:44 -0400

    Set conditional rendering if link  not available

--- src/components/Scribble/Scribble.js
+++ src/components/Scribble/Scribble.js
@@ -2,13 +2,21 @@ import React from 'react';
 
 import {Ref} from '../Ref/Ref';
 
+import loading from "../../loading.png";
+
 const Scribble = (props) => {
 
     return(
         // <div className="refs scribble">
         <Ref>
-            <a href = {'https://www.instagram.com/startrefio/'} target="_blank"><img src = {props.scribbleUrl} alt = "Scribble"></img></a>
+            <a href = {'https://www.instagram.com/startrefio/'} target="_blank"><img src = {props.scribbleUrl ? props.scribbleUrl : loading} alt = "Scribble"></img></a>
             {/* <p className = "author">by <a href={props.photoInfo.user.links.html + "?utm_source=startref&utm_medium=referral"} target="_blank"> {props.photoInfo.user.name} </a></p> */}
+            {
+                props.scribbleUrl ? 
+                <p className = "author"></p>
+                :
+                <p className = "author">follow <a href="https://www.patreon.com/xanderjakeq" target="_blank">xanderjakeq</a></p>
+            }
         </Ref>
     );
 }
