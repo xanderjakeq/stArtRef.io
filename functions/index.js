3efed8d997a5649841906ca7992fc5f6e7a73dfb
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-08-14 16:51:08 -0400

    Move createUserAccount functionality under components

--- functions/index.js
+++ functions/index.js
@@ -1,40 +1,5 @@
-const functions = require('firebase-functions');
-const firebase = require('firebase-admin')
-const express = require('express');
-
 const api = require('./components/api');
-
-//not needed
-
-const firebaseApp = firebase.initializeApp(
-    functions.config().firebase
-);
-
-const database = firebase.database();
+const createUserAccount = require('./components/createUserAccount');
 
 exports.api = api;
-
-exports.createUserAccount = functions.auth.user().onCreate(user => {
-    const name = user.displayName;
-    const uid = user.uid;
-    const email = user.email;
-    const username = email.substring(0,email.indexOf('@'));
-    const photoUrl = user.photoURL || 'some default link';
-
-
-    const newUserRef = database.ref().child(`Users/${uid}`);
-    newUserRef.set({
-        name: name,
-        email: email,
-        photoUrl: photoUrl,
-        username: username,
-        uid: uid,
-        website:''
-    });
-
-    database.ref().child(`Usernames/`).update({
-        [uid]:username
-    })
-
-    return
-});
+exports.createUserAccount = createUserAccount;
