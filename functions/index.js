8376ec213d2888676bd11262d6589cec0521fd52
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-08-14 16:04:39 -0400

    Move api implementation under components

--- functions/index.js
+++ functions/index.js
@@ -2,33 +2,17 @@ const functions = require('firebase-functions');
 const firebase = require('firebase-admin')
 const express = require('express');
 
+const api = require('./components/api');
+
 //not needed
 
 const firebaseApp = firebase.initializeApp(
     functions.config().firebase
 );
 
-
-const app = express();
 const database = firebase.database();
 
-
-app.get('/*', (request, response) => {
-    console.log('main');
-    response.sendFile(__dirname + '/build/index.html');
-})
-app.get('/account', (request, response) => {
-    console.log('account');
-    response.sendFile( __dirname + '/views/userProfile.html');
-})
-
-app.use(function (req, res, next) {
-    console.log('404');
-    res.status(404).sendFile(__dirname + '/views/404.html');
-  })
-
-exports.app = functions.https.onRequest(app);
-
+exports.api = api;
 
 exports.createUserAccount = functions.auth.user().onCreate(user => {
     const name = user.displayName;
